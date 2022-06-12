fecaf93db66c26b8a187712ba3666056b890f097
Ebenezer NIKABOU <nikaboue10@gmail.com>
2022-06-12 19:16:45 +0000

    added homepage skeloton

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,10 @@ class DefaultController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(): Response
+    public function index(TagRepository $tagRepository): Response
     {
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+            'tags' => $tagRepository->findAll(),
         ]);
     }
 }
